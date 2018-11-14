853318b8f7b9b4da67599d4791e5d88aa54ade37
Ravi Raj <ravirajfromindia@gmail.com>
2018-11-14 17:56:46 +0530

    Successfully completed to open the page on click by user

--- onlineshopping/src/main/java/com/shopping/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/shopping/onlineshopping/controller/PageController.java
@@ -8,30 +8,48 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-	@RequestMapping(value={"/","/home","/index"})
-	public ModelAndView index(){
-		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring Web MVC");
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
+
 		return mv;
 	}
-/*	@RequestMapping(value="/test")
-	public ModelAndView test(@RequestParam(value="greeting",required=false)String greetings){
-		if(greetings==null){
-			greetings="Please, you must provide the greeting value";
-		}
-		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting", greetings);
+
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+
 		return mv;
-		
 	}
-	@RequestMapping(value="/test1/{greeting}")
-	public ModelAndView test1(@PathVariable(value="greeting")String greetings){
-		if(greetings==null){
-			greetings="Please, you must provide the greeting value";
-		}
-		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting", greetings);
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+
 		return mv;
-		
-	}*/
+	}
+
+	/*
+	 * @RequestMapping(value="/test") public ModelAndView
+	 * test(@RequestParam(value="greeting",required=false)String greetings){
+	 * if(greetings==null){ greetings=
+	 * "Please, you must provide the greeting value"; } ModelAndView mv=new
+	 * ModelAndView("page"); mv.addObject("greeting", greetings); return mv;
+	 * 
+	 * }
+	 * 
+	 * @RequestMapping(value="/test1/{greeting}") public ModelAndView
+	 * test1(@PathVariable(value="greeting")String greetings){
+	 * if(greetings==null){ greetings=
+	 * "Please, you must provide the greeting value"; } ModelAndView mv=new
+	 * ModelAndView("page"); mv.addObject("greeting", greetings); return mv;
+	 * 
+	 * }
+	 */
 }
